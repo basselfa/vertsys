575c44a9039668e1853a969e52eeb2f9f4ea09f0
basselfa <38949665+basselfa@users.noreply.github.com>
2019-06-03 19:56:08 +0200

    Update RequestListMessage.java

--- HA3/code/common/de/tu_berlin/cit/vs/jms/common/RequestListMessage.java
+++ HA3/code/common/de/tu_berlin/cit/vs/jms/common/RequestListMessage.java
@@ -2,7 +2,13 @@ package de.tu_berlin.cit.vs.jms.common;
 
 
 public class RequestListMessage extends BrokerMessage {
-    public RequestListMessage() {
+	private String clientName;
+    public RequestListMessage(String clientName) {
         super(Type.STOCK_LIST);
+        this.clientName = clientName;
     }
+    public String getClientName() {
+        return clientName;
+    }
+    
 }
